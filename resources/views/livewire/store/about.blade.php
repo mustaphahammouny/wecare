336c49f3d96bdeeb2547f64121fa9eda69d9fb3e
KJR <mustapha.hammouny@gmail.com>
2023-10-14 19:53:59 +0200

    about

---
+++ resources/views/livewire/store/about.blade.php
@@ -0,0 +1,51 @@
+<div>
+    <x-breadcumb title="About" />
+
+    <div class="container py-5">
+        <div class="row wow fadeInUp" data-wow-delay="100ms">
+            <div class="col">
+                <h4 class="title">@lang('About ' . config('app.name'))</h4>
+                <p>
+                    @lang('Welcome to ' . config('app.name') . ', your one-stop solution for all your service needs. At ' . config('app.name') . ', we understand the value of time, and we\'re here to make your life easier. Our mission is to connect you with skilled and trusted professionals who offer a wide range of services, from cleaning and cooking to teaching and coaching.')
+                </p>
+
+                <h4 class="title">@lang('Our Story')</h4>
+                <p>
+                    @lang('Founded with a passion for convenience and a commitment to quality, ' . config('app.name') . ' started as a small idea to simplify your everyday life. We recognized the challenges people face when searching for reliable service providers, and we set out to change that.')
+                </p>
+                <p>
+                    @lang('Our journey began with a belief that technology could bridge the gap between service seekers and providers. Today, we are proud to say that we have created a thriving community of users and professionals who trust ' . config('app.name') . ' to fulfill their needs.')
+                </p>
+
+                <h4 class="title">@lang('What Sets Us Apart')</h4>
+                <ul>
+                    <li>
+                        <strong>@lang('Diverse Services'):</strong> @lang('We offer an extensive array of services. Whether you\'re looking for a house cleaner, a personal chef, a language tutor, or a fitness coach, you can find the perfect professional right here.')
+                    </li>
+                    <li>
+                        <strong>@lang('Vetted Professionals'):</strong> @lang('Booking a service is as simple as a few taps on your smartphone. We\'ve designed our app with user-friendliness in mind to make the booking process quick and hassle-free.')
+                    </li>
+                    <li>
+                        <strong>@lang('Easy Booking'):</strong> @lang('Your peace of mind is our priority. Every service provider on our platform is carefully vetted, background-checked, and rated by our community, ensuring you receive top-notch service every time.')
+                    </li>
+                    <li>
+                        <strong>@lang('Transparent Pricing'):</strong> @lang('No hidden costs. You\'ll always know what you\'re paying for upfront. Choose from different pricing options that suit your budget.')
+                    </li>
+                    <li>
+                        <strong>@lang('Customer Support'):</strong> @lang('We\'re here for you around the clock. Our dedicated customer support team is ready to assist with any questions or concerns you may have.')
+                    </li>
+                </ul>
+
+                <h4 class="title">@lang('Our Vision')</h4>
+                <p>
+                    @lang('We envision a world where everyone can easily access the services they need to improve their quality of life. Whether it\'s maintaining a clean home, learning a new skill, or achieving personal fitness goals, [Your App Name] strives to be the bridge between your aspirations and the experts who can help you achieve them.')
+                </p>
+
+                <h4 class="title">@lang('Join the ' . config('app.name') . ' Community')</h4>
+                <p>
+                    @lang('Become a part of the growing ' . config('app.name') . ' community and experience a new level of convenience, reliability, and quality service. Say goodbye to the hassle of finding the right service provider. We\'re here to make your life better.')
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
