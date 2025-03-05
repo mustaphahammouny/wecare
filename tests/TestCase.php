6d56175b142741c1c7300ff899a878acf349f0cf
Mustapha <mustapha.hammouny@gmail.com>
2025-03-05 22:48:04 +0000

    fix: disable vite on testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
